ba9eaf85bbca4ab506ab5df34a37505ff0f75185
Wesley Chestang <wesleychestang@gmail.com>
2018-09-28 20:09:16 -0500

    Example Component Use
    Illustrating the concepts of...
    components input, and output
    
    Calling components in pages
    
    Passing data into a component dynamically
    
    passing data $events into a function and defining it in a typescript file

--- src/components/test/test.ts
+++ src/components/test/test.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 /**
  * Generated class for the TestComponent component.
@@ -12,11 +12,32 @@ import { Component } from '@angular/core';
 })
 export class TestComponent {
 
+  //Supply an Input called 'myText' and asign it to textToUse
+  @Input('myText') textToUse;
+
+  //Supply and Output called 'somethingHappened'
+  //This Output is of the same name as the custom event we created on the home.ts 
+  // you then set this equal to an event emitter, so the component can now detect that event occuring 
+  @Output() somethingHappened = new EventEmitter();
+
   text: string;
 
   constructor() {
     console.log('Hello TestComponent Component');
-    this.text = 'It works! What up son!';
+    this.text = 'It works! What up son! ';
+  }
+
+  //ngAfterViewInit is a method that waits for everything to load before it executes 
+  ngAfterViewInit(){
+    //Set this.text to 'textToUse' ... which was given its value through the input parameter above
+    //The input parameter is being given a value from whichever page passes one into it when the component is called 
+    this.text = this.textToUse;
+
+    //For testing sake
+    //Set an interval to 3seconds ... 3000ms ... and emit a value every time it fires 
+    let interval = setInterval(() => {
+      this.somethingHappened.emit("its time");
+    }, 3000);
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  // We can set the dynamic variable from the home.html here ...
+  myHomeText: any = "this is the home page";
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  //Log out the data from the $event passing through the function 
+  doSomething(ev){
+    console.log(ev);
+  }
 }
