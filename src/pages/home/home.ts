8638412e8c510688e19cd44564428881b2fce1b7
lcb1001 <lcb1001@jagmail.southalabama.edu>
2018-10-23 22:16:15 -0500

    Updates and more Updates
    Shoved in some CSS and images

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,7 +3,8 @@ import { NavController } from 'ionic-angular';
 import { CalendarPage } from '../calendar/calendar';
 
 //Import any other pages you need to reference
-import { TestPage } from '../test/test';
+ import { TestPage } from '../test/test';
+ import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-home',
@@ -20,6 +21,14 @@ export class HomePage {
   }
   
   goToCalendar() {
-    this.navCtrl.setRoot(CalendarPage);
+    this.navCtrl.push(CalendarPage);
+  }
+
+  goToHelp() {
+    this.navCtrl.push(AboutPage);
+  }
+
+  goToBookShelf(){
+    window.open("https://abdodigital.com/?tk=A8A412886D6FFCA174EA2F2E90A6169D", '_system', 'location=yes');
   }
 }
